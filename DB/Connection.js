2500d258580a907b104d8b954e74c798d5f4e62c
youssefElkilany <elkilany17@gmail.com>
2024-07-19 15:45:30 +0300

    connecting to cloud database

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const connectDB = async()=>{
-    return await mongoose.connect("mongodb://0.0.0.0:27017/Cinema").then(()=>{
+    return await mongoose.connect(process.env.DB_ATLAS).then(()=>{
         console.log("DB is connected")
     }).catch(err=>{
         console.log(`DB is not connected`)
