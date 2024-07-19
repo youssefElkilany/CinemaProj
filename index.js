5d2a1d175fa1c63c580b4d289e26688e794b25d7
youssefElkilany <elkilany17@gmail.com>
2024-07-19 15:05:23 +0300

    chnages to deploy on vercel

--- index.js
+++ index.js
@@ -20,10 +20,8 @@ bootstrap(app,express)
 // sha256 from https://cdnjs.cloudflare.com/ajax/libs/js-sha256/0.9.0/sha256.min.js
 //axios from https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js
 
+const port = process.env.PORT || 5000
 
-        
-                        
-
-app.listen(5000,()=>{
-    console.log(`server is running on 5000`)
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`)
 })
